a1e9d9bf965dfb9a3e3ee574062e3fe2a71403f1
tom <tmullen1991@gmail.com>
2021-07-05 16:41:00 -0600

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Staff from "./components/staff";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="contnet-center">
+        <Staff />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/staff/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./styles.css";
+
+class Staff extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <svg>
+          <rect />
+        </svg>
+        <svg>
+          <rect />
+        </svg>
+        <svg>
+          <rect />
+        </svg>
+        <svg>
+          <rect />
+        </svg>
+        <svg>
+          <rect />
+        </svg>
+      </div>
+    );
+  }
+}
+
+export default Staff;
